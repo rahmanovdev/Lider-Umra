a734205f7b509c23246e39d89023b16e12a36b07
Janybek2007 <islam.janybekov.007@gmail.com>
2025-03-26 13:54:50 +0600

    🔍 Improve SEO: Updated robots.txt with better indexing rules and structured sitemap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,83 +8,84 @@ import { NextIntlClientProvider } from 'next-intl';
 import { RootProvider } from '@/providers';
 import { APP_URL } from '@/constants/url.constants';
 import { SITE_KEYWORDS, SITE_NAME } from '@/constants/seo.constants';
+import { Metadata } from 'next';
+
 const AppUrl = APP_URL ? new URL(APP_URL) : null;
 
-export const generateMetadata = async () => {
+export const generateMetadata = async (): Promise<Metadata> => {
    const locale = await getLocale();
    const t = await getTranslations();
 
    return {
-      title: {
-         absolute: SITE_NAME,
-         template: `%s - ${SITE_NAME}`,
-      },
+      title: SITE_NAME,
       description: t('descriptions.base'),
       metadataBase: AppUrl,
       applicationName: SITE_NAME,
       keywords: SITE_KEYWORDS,
-      generator: 'Next.js',
+      generator: 'iant.kg',
       creator: 'Лидер Умра',
       publisher: 'Лидер Умра',
-      robots: {
-         index: true,
-         follow: true,
-         googleBot: { index: true, follow: true },
-      },
-      icons: {
-         icon: `${AppUrl}/logo.png`,
-         shortcut: `${AppUrl}/logo.png`,
-         apple: `${AppUrl}/logo.png`,
-      },
+      alternates: { canonical: `${APP_URL}` },
+      icons: [
+         { type: 'image/png', url: '/seo/favicon-16x16.png', sizes: '16x16' },
+         { type: 'image/png', url: '/seo/favicon-32x32.png', sizes: '32x32' },
+         { type: 'icon', url: '/seo/favicon.ico', sizes: '32x32' },
+         {
+            type: 'image/png',
+            url: '/seo/apple-touch-icon.png',
+            sizes: '180x180',
+         },
+         { type: 'image/png', url: '/seo/favicon.png', sizes: 'any' },
+         {
+            type: 'image/png',
+            url: '/seo/android-chrome-192x192.png',
+            sizes: '192x192',
+         },
+         {
+            type: 'image/png',
+            url: '/seo/android-chrome-512x512.png',
+            sizes: '512x512',
+         },
+      ],
       openGraph: {
          title: SITE_NAME,
          description: t('descriptions.base'),
          type: 'website',
-         emails: [],
+         url: `${APP_URL}`,
+         siteName: 'Лидер Умра',
          locale: locale === 'ru' ? 'ru_RU' : 'kg_KG',
+         countryName: 'Kyrgyzstan',
          images: [
-            {
-               url: `${AppUrl}/logo.png`,
-               width: '192',
-               height: '192',
-               alt: 'Лидер Умра - Меккеге Зыярат',
-            },
-            {
-               url: `${AppUrl}/logo.png`,
-               width: 1200,
-               height: 630,
-               alt: 'Лидер Умра - Меккеге Зыярат',
-            },
+            { url: '/seo/android-chrome-192x192.png', width: 192, height: 192 },
+            { url: '/seo/android-chrome-512x512.png', width: 512, height: 512 },
+            { url: '/seo/apple-touch-icon.png', width: 180, height: 180 },
          ],
-         ...(APP_URL ? { url: new URL(APP_URL) } : {}),
       },
       twitter: {
+         card: 'summary_large_image',
          title: SITE_NAME,
          description: t('descriptions.base'),
          images: [
-            {
-               url: `${AppUrl}/logo.png`,
-               width: '192',
-               height: '192',
-               alt: 'Лидер Умра - Меккеге Зыярат',
-            },
-            {
-               url: `${AppUrl}/logo.png`,
-               width: 1200,
-               height: 630,
-               alt: 'Лидер Умра - Меккеге Зыярат',
-            },
+            { url: '/seo/android-chrome-192x192.png', width: 192, height: 192 },
+            { url: '/seo/android-chrome-512x512.png', width: 512, height: 512 },
          ],
-         ...(APP_URL ? { url: new URL(APP_URL) } : {}),
+      },
+      robots: {
+         index: true,
+         follow: true,
+         'max-snippet': -1,
+         'max-image-preview': 'large',
+         'max-video-preview': -1,
+      },
+      other: {
+         'theme-color': '#ffffff',
+         'msapplication-TileColor': '#ffffff',
+         'msapplication-TileImage': '/seo/android-chrome-192x192.png',
       },
    };
 };
 
-const interSans = Inter({
-   variable: '--font-inter',
-   subsets: ['latin'],
-});
-
+const interSans = Inter({ variable: '--font-inter', subsets: ['latin'] });
 const montserrat = Montserrat({
    variable: '--font-montserrat-sans',
    subsets: ['latin'],
@@ -95,9 +96,31 @@ export default async function RootLayout({
 }: Readonly<React.PropsWithChildren>) {
    const locale = await getLocale();
    const messages = await getMessages();
+
    return (
       <html lang={locale} suppressHydrationWarning>
-         <body className={`${interSans.variable}  ${montserrat.variable}`}>
+         <head>
+            <script
+               type='application/ld+json'
+               dangerouslySetInnerHTML={{
+                  __html: JSON.stringify({
+                     '@context': 'https://schema.org',
+                     '@type': 'WebSite',
+                     name: SITE_NAME,
+                     url: APP_URL,
+                     publisher: {
+                        '@type': 'Organization',
+                        name: 'Лидер Умра',
+                        logo: {
+                           '@type': 'ImageObject',
+                           url: `${APP_URL}/seo/android-chrome-192x192.png`,
+                        },
+                     },
+                  }),
+               }}
+            />
+         </head>
+         <body className={`${interSans.variable} ${montserrat.variable}`}>
             <div className='wrapper'>
                <NextIntlClientProvider messages={messages}>
                   <RootProvider>{children}</RootProvider>
